c5dc9f696668ef67a4f308a1b18399206c01e499
Ben Hodgson <github@benhodgson.net>
2021-11-08 19:14:18 +0000

    Create base NextJS app

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Loddon Social Enterprise</title>
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to Loddon Social Enterprise
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
