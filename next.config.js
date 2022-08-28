5478480ac67a58fe6d74cfca6a82e380dae6bb89
Ben Hodgson <github@benhodgson.net>
2022-08-28 14:42:32 +0100

    Add domain to image loader

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   images: {
+    domains: ['https://loddon-social-enterprise.github.io/'],
     loader: 'akamai',
     path: '',
   },
