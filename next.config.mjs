1585eea024de509579bf5ed84d5c9ab963fb1815
Ben Hodgson <github@benhodgson.net>
2022-08-28 14:36:31 +0100

    Use Akamai image loader

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
   assetPrefix: isProd ? '/website/' : '',
   reactStrictMode: true
 };
