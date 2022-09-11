19e55634466bd2e67f9242d59fca19496e2b6787
Ben Hodgson <github@benhodgson.net>
2022-09-11 14:37:08 +0100

    Add redirects for existing site urls

--- next.config.js
+++ next.config.js
@@ -7,5 +7,34 @@ module.exports = {
     path: '.',
   },
   reactStrictMode: true,
-  poweredByHeader: false
+  poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: '/our-services',
+        destination: '/what-we-do',
+        permanent: true,
+      },
+      {
+        source: '/our-users',
+        destination: '/our-service-users',
+        permanent: true,
+      },
+      {
+        source: '/ready-made-picture-frame-company-quote',
+        destination: '/our-partners',
+        permanent: true
+      },
+      {
+        source: '/meet-the-team',
+        destination: '/our-team',
+        permanent: true,
+      },
+      {
+        source: '/news-events',
+        destination: '/',
+        permanent: true,
+      }
+    ]
+  },
 };
