9224bdfb623e9e0063babf7b233470853267b983
Ben Hodgson <github@benhodgson.net>
2022-09-18 12:39:43 +0100

    Remove rogue script tags inside counter code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -72,7 +72,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         id="counter-analytics"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `<script>if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,id:"2a772c3b-150f-4721-9a77-8a8cb09fdeba",utcoffset:"1"}))};sessionStorage.setItem("_swa","1");</script>`
+          __html: `if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,id:"2a772c3b-150f-4721-9a77-8a8cb09fdeba",utcoffset:"1"}))};sessionStorage.setItem("_swa","1");`
         }}
       />
     </>
