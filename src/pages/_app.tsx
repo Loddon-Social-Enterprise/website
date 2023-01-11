c3c0f0888263f8fbc877117973b72881437fe83e
Ben Hodgson <github@benhodgson.net>
2023-01-11 21:25:06 +0000

    Add meta viewport tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -44,6 +44,10 @@ const App = ({ Component, pageProps }: AppProps) => {
         ]}
         additionalMetaTags={[
           {
+            name: 'viewport',
+            content: 'width=device-width,initial-scale=1'
+          },
+          {
             name: 'apple-mobile-web-app-title',
             content: 'Loddon Social Enterprise'
           },
