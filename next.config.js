78ce8a1f49e56b95796a05980358f6897c3a2395
Ben Hodgson <github@benhodgson.net>
2023-01-11 21:13:34 +0000

    Fix incorrect next config phase check

--- next.config.js
+++ next.config.js
@@ -3,45 +3,47 @@ const {
   PHASE_DEVELOPMENT_SERVER
 } = require('next/constants')
 
-const env = phase === PHASE_DEVELOPMENT_SERVER ? 'development' : 'production';
+module.exports = (phase) => {
+  const env = phase === PHASE_DEVELOPMENT_SERVER ? 'development' : 'production';
 
-module.exports = {
-  images: {
-    loader: 'akamai',
-    path: '.',
-  },
-  reactStrictMode: true,
-  poweredByHeader: false,
-  env: {
-    NEXT_PUBLIC_ENV: env
-  },
-  async redirects() {
-    return [
-      {
-        source: '/our-services',
-        destination: '/what-we-do',
-        permanent: true,
-      },
-      {
-        source: '/our-users',
-        destination: '/our-service-users',
-        permanent: true,
-      },
-      {
-        source: '/ready-made-picture-frame-company-quote',
-        destination: '/our-partners',
-        permanent: true
-      },
-      {
-        source: '/meet-the-team',
-        destination: '/our-team',
-        permanent: true,
-      },
-      {
-        source: '/news-events',
-        destination: '/',
-        permanent: true,
-      }
-    ]
-  },
+  return {
+    images: {
+      loader: 'akamai',
+      path: '.',
+    },
+    reactStrictMode: true,
+    poweredByHeader: false,
+    env: {
+      NEXT_PUBLIC_ENV: env
+    },
+    async redirects() {
+      return [
+        {
+          source: '/our-services',
+          destination: '/what-we-do',
+          permanent: true,
+        },
+        {
+          source: '/our-users',
+          destination: '/our-service-users',
+          permanent: true,
+        },
+        {
+          source: '/ready-made-picture-frame-company-quote',
+          destination: '/our-partners',
+          permanent: true
+        },
+        {
+          source: '/meet-the-team',
+          destination: '/our-team',
+          permanent: true,
+        },
+        {
+          source: '/news-events',
+          destination: '/',
+          permanent: true,
+        }
+      ]
+    },
+  };
 };
