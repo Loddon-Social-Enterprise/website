60870495d5402de7a8e821841fc1a231a6bcd43d
Ben Hodgson <189707+benhodgson87@users.noreply.github.com>
2023-07-23 18:52:52 +0100

    fix: Remove unnecessary duplicate page for 'what-we-do' (#100)

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,12 +1,25 @@
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { documentToReactComponents, Options } from '@contentful/rich-text-react-renderer';
+import { INLINES } from '@contentful/rich-text-types';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { getGenericPage } from 'src/api/queries';
 import styles from 'src/styles/pages/page.module.scss';
 
 interface Context {
   params: {
     slug: string;
+  };
+}
+
+function renderOptions(): Options {
+  return {
+    renderNode: {
+      [INLINES.HYPERLINK]: ({ data }, children) => {
+        const content = children as Array<ReactNode>; // Type for hyperlinks in Options is incorrect, so we need to cast this
+        return <>{content.length > 0 && <Link href={data.uri}>{content[0]}</Link>}</>;
+      }
+    }
   };
 }
 
@@ -23,5 +36,5 @@ export default async function Page({ params }: Context) {
   const { body } = await getGenericPage(params.slug);
   if (!body) return notFound();
 
-  return <section className={styles.page}>{body && documentToReactComponents(body.json)}</section>;
+  return <section className={styles.page}>{body && documentToReactComponents(body.json, renderOptions() || {})}</section>;
 }
